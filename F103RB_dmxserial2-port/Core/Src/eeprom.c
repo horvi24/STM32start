7356ad862aa4df2bdd108f435e514ada2000d6e0
h24 <h24@DESKTOP-2ASMVSI>
2024-04-24 22:09:43 +0200

    ...eprom error

---
+++ F103RB_dmxserial2-port/Core/Src/eeprom.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include "eeprom.h"
+
+
+uint16_t readEEPROMHalfWord(uint16_t addr){
+    return 1234;
+}
+
+uint16_t writeEEPROMHalfWord(uint16_t addr, uint16_t data){
+    return 5678;
+}
+
+void enableEEPROMWriting(void){
+
+}
+
+void disableEEPROMWriting(void){
+
+}
