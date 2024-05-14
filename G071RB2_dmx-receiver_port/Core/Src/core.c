50b00698e4b86a04f1a5867927e3d173f38fe0e5
horvi24 <119654125+horvi24@users.noreply.github.com>
2024-05-14 12:03:42 +0200

    DMX & PWM RGBW LED working ok

--- G071RB2_dmx-receiver_port/Core/Src/core.c
+++ G071RB2_dmx-receiver_port/Core/Src/core.c
@@ -13,6 +13,8 @@
 #define ADC_CHANNELS 		2
 #define ADC_MAX 		1800
 
+#define ADDR_LED_RGBW	1
+
 //-h24 extern ADC_HandleTypeDef hadc1;
 extern TIM_HandleTypeDef htim3;
 
@@ -55,18 +57,20 @@ void core_process(void)
 
 	len = dmx_receive(packet);
 
-	led_set(0, curve_fn(packet[1]));
-	led_set(1, curve_fn(packet[2]));
-	led_set(2, curve_fn(packet[3]));
-	led_set(3, curve_fn(packet[4]));
+	led_set(0, curve_fn(packet[ADDR_LED_RGBW]));
+	led_set(1, curve_fn(packet[ADDR_LED_RGBW+1]));
+	led_set(2, curve_fn(packet[ADDR_LED_RGBW+2]));
+	led_set(3, curve_fn(packet[ADDR_LED_RGBW+3]));
 /*-h24
 	if ((adc[0] < ADC_MAX) && (adc[1] < ADC_MAX))
 		usb_dumppacket(packet, len);
 	else
 //-h24		usb_printf("DMX.Len=%d ADC1=%d ADC2=%d\r\n", len, adc[0], adc[1]);
  */
-	if(!HAL_GPIO_ReadPin(SW_BLUE_GPIO_Port, SW_BLUE_Pin))
-      //printf("/%3d/ %3d %3d %3d %3d\r\n", len, packet[1], packet[2], packet[3], packet[4]);
-	  usb_dumppacket(packet, len);
+	if(!HAL_GPIO_ReadPin(SW_BLUE_GPIO_Port, SW_BLUE_Pin)){
+	      printf("/%3d/ %3d %3d %3d %3d\r\n", ADDR_LED_RGBW, packet[ADDR_LED_RGBW], packet[ADDR_LED_RGBW+1], packet[ADDR_LED_RGBW+2], packet[ADDR_LED_RGBW+3]);
+		  usb_dumppacket(packet, len);
+
+	}
 
 }
