150ccf7b50a58dca107e608630c14dd973a1c4c7
horvi24 <119654125+horvi24@users.noreply.github.com>
2024-05-30 11:05:31 +0200

    v1.1 cleaned code

--- G071RB_dmx-master-clean/Core/Src/stm32g0xx_hal_msp.c
+++ G071RB_dmx-master-clean/Core/Src/stm32g0xx_hal_msp.c
@@ -192,7 +192,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     PA9     ------> USART1_TX
     PA10     ------> USART1_RX
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+    GPIO_InitStruct.Pin = DMX_TX1_Pin|DMX_RX1_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -226,7 +226,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     PA2     ------> USART2_TX
     PA3     ------> USART2_RX
     */
-    GPIO_InitStruct.Pin = USART2_TX_Pin|USART2_RX_Pin;
+    GPIO_InitStruct.Pin = DBG_TX2_Pin|DBG_RX2_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -260,7 +260,7 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
     PA9     ------> USART1_TX
     PA10     ------> USART1_RX
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9|GPIO_PIN_10);
+    HAL_GPIO_DeInit(GPIOA, DMX_TX1_Pin|DMX_RX1_Pin);
 
   /* USER CODE BEGIN USART1_MspDeInit 1 */
 
@@ -278,7 +278,7 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
     PA2     ------> USART2_TX
     PA3     ------> USART2_RX
     */
-    HAL_GPIO_DeInit(GPIOA, USART2_TX_Pin|USART2_RX_Pin);
+    HAL_GPIO_DeInit(GPIOA, DBG_TX2_Pin|DBG_RX2_Pin);
 
   /* USER CODE BEGIN USART2_MspDeInit 1 */
 
